b98bb3397e5da226355642ac438b2092113e2cee
Michael Taylor <michael@emte.net.au>
2023-01-31 13:09:31 +1100

    Cache compile requests
    Store results of compile requests to speed up repeated experiments

--- experiments/src/quartus.erl
+++ experiments/src/quartus.erl
@@ -2,6 +2,8 @@
 
 -export([compile/1]).
 
+-define(SEPARATOR, <<"\n====================\n">>).
+
 %%====================================================================
 %% compile
 %%====================================================================
@@ -10,8 +12,22 @@ compile(Request = #{device := Device, settings := Settings, vhdl := VHDL})
         when is_binary(Device) andalso
              is_binary(Settings) andalso
              is_binary(VHDL) ->
+    With = cache_with(Device, Settings, VHDL),
+    CacheDir = cache_dir(With),
+    case cache_read_with(CacheDir) of
+        {ok, With} ->
+            cache_hit(CacheDir);
+
+        {error, enoent} ->
+            cache_miss(With, CacheDir, Request)
+    end.
+
+%%--------------------------------------------------------------------
+
+compile_request(Request) ->
     compile_connect(),
-    compile_result(gen_server:call({global, quartus}, {compile, Request})).
+    Response = gen_server:call({global, quartus}, {compile, Request}, 10000),
+    compile_response(Response).
 
 %%--------------------------------------------------------------------
 
@@ -31,15 +47,15 @@ compile_connect() ->
 
 %%--------------------------------------------------------------------
 
-compile_result({ok, POF}) ->
-    pof_file:decode(POF);
-compile_result({error, {quartus_map, Exit, Out}}) ->
+compile_response({ok, POF}) ->
+    {ok, POF};
+compile_response({error, {quartus_map, Exit, Out}}) ->
     compile_exit("MAP", Exit, Out);
-compile_result({error, {quartus_fit, Exit, Out}}) ->
+compile_response({error, {quartus_fit, Exit, Out}}) ->
     compile_exit("FIT", Exit, Out);
-compile_result({error, {quartus_asm, Exit, Out}}) ->
+compile_response({error, {quartus_asm, Exit, Out}}) ->
     compile_exit("ASM", Exit, Out);
-compile_result({error, Error}) ->
+compile_response({error, Error}) ->
     io:format("QUARTUS: ~p~n", [Error]),
     error.
 
@@ -51,4 +67,76 @@ compile_exit(Who, Exit, Out) ->
     io:format("========================================~n", []),
     io:format("QUARTUS ~s: exit # ~p~n", [Who, Exit]),
     error.
+
+%%--------------------------------------------------------------------
+
+cache_with(Device, Settings, VHDL) ->
+    iolist_to_binary([
+        Device,
+        <<"\n">>,
+        Settings,
+        ?SEPARATOR,
+        VHDL
+    ]).
+
+%%--------------------------------------------------------------------
+
+cache_dir(With) ->
+    Hash = crypto:hash(sha256, With),
+    Base64 = base64url:encode(Hash),
+    cache_make_dir(filename:join("cache", Base64)).
+
+%%--------------------------------------------------------------------
+
+cache_make_dir(CacheDir) ->
+    case file:make_dir(CacheDir) of
+        ok ->
+            CacheDir;
+
+        {error, eexist} ->
+            CacheDir
+    end.
+
+%%--------------------------------------------------------------------
+
+cache_read_pof(CacheDir) ->
+    File = filename:join(CacheDir, "experiment.pof"),
+    file:read_file(File).
+
+%%--------------------------------------------------------------------
+
+cache_read_with(CacheDir) ->
+    File = filename:join(CacheDir, "with"),
+    file:read_file(File).
+
+%%--------------------------------------------------------------------
+
+cache_hit(CacheDir) ->
+    {ok, POF} = cache_read_pof(CacheDir),
+    pof_file:decode(POF).
+
+%%--------------------------------------------------------------------
+
+cache_miss(With, CacheDir, Request) ->
+    case compile_request(Request) of
+        {ok, POF} ->
+            cache_write_pof(CacheDir, POF),
+            cache_write_with(CacheDir, With),
+            pof_file:decode(POF);
+
+        Error ->
+            Error
+    end.
+
+%%--------------------------------------------------------------------
+
+cache_write_pof(CacheDir, POF) ->
+    File = filename:join(CacheDir, "experiment.pof"),
+    ok = file:write_file(File, POF).
+
+%%--------------------------------------------------------------------
+
+cache_write_with(CacheDir, With) ->
+    File = filename:join(CacheDir, "with"),
+    ok = file:write_file(File, With).
 
