53fe58c106e09021df48b2f88cdb3a8bf267c9a4
Michael Taylor <michael@emte.net.au>
2023-02-03 13:53:07 +1100

    Add lab() and lc() types

--- experiments/src/density.erl
+++ experiments/src/density.erl
@@ -4,6 +4,7 @@
 -export([devices/1]).
 -export([fuse_count/1]).
 -export([or_device/1]).
+-export([labs/1]).
 
 -export_type([density/0]).
 
@@ -14,6 +15,7 @@
     epm2210.
 
 -type device() :: device:device().
+-type lab() :: lab:lab().
 
 %%====================================================================
 %% list
@@ -84,4 +86,127 @@ or_device(Density = epm1270) -> Density;
 or_device(Density = epm2210) -> Density;
 or_device(Device) ->
     device:density(Device).
+
+%%====================================================================
+%% labs
+%%====================================================================
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+
+fuses_test() ->
+    ?assertEqual(24, length(labs(epm240))),
+    ?assertEqual(57, length(labs(epm570))),
+    ?assertEqual(127, length(labs(epm1270))),
+    ?assertEqual(221, length(labs(epm2210))).
+
+-endif.
+
+-spec labs(density()) -> [lab()].
+
+-define(COLUMN4(X, Y),
+    {lab, X, Y - 0},
+    {lab, X, Y - 1},
+    {lab, X, Y - 2},
+    {lab, X, Y - 3}
+).
+-define(COLUMN7(X, Y),
+    {lab, X, Y - 0},
+    {lab, X, Y - 1},
+    {lab, X, Y - 2},
+    {lab, X, Y - 3},
+    {lab, X, Y - 4},
+    {lab, X, Y - 5},
+    {lab, X, Y - 6}
+).
+-define(COLUMN10(X, Y),
+    {lab, X, Y - 0},
+    {lab, X, Y - 1},
+    {lab, X, Y - 2},
+    {lab, X, Y - 3},
+    {lab, X, Y - 4},
+    {lab, X, Y - 5},
+    {lab, X, Y - 6},
+    {lab, X, Y - 7},
+    {lab, X, Y - 8},
+    {lab, X, Y - 9}
+).
+-define(COLUMN13(X, Y),
+    {lab, X, Y - 0},
+    {lab, X, Y - 1},
+    {lab, X, Y - 2},
+    {lab, X, Y - 3},
+    {lab, X, Y - 4},
+    {lab, X, Y - 5},
+    {lab, X, Y - 6},
+    {lab, X, Y - 7},
+    {lab, X, Y - 8},
+    {lab, X, Y - 9},
+    {lab, X, Y - 10},
+    {lab, X, Y - 11},
+    {lab, X, Y - 12}
+).
+
+labs(epm240) ->
+    [?COLUMN4(2, 4),
+     ?COLUMN4(3, 4),
+     ?COLUMN4(4, 4),
+     ?COLUMN4(5, 4),
+     ?COLUMN4(6, 4),
+     ?COLUMN4(7, 4)
+    ];
+labs(epm570) ->
+    [?COLUMN4(1, 7),
+     ?COLUMN4(2, 7),
+     ?COLUMN4(3, 7),
+     ?COLUMN4(4, 7),
+     ?COLUMN4(5, 7),
+     ?COLUMN4(6, 7),
+     ?COLUMN4(7, 7),
+     ?COLUMN4(8, 7),
+     ?COLUMN4(9, 7),
+     ?COLUMN7(10, 7),
+     ?COLUMN7(11, 7),
+     ?COLUMN7(12, 7)
+    ];
+labs(epm1270) ->
+    [?COLUMN7(1, 10),
+     ?COLUMN7(2, 10),
+     ?COLUMN7(3, 10),
+     ?COLUMN7(4, 10),
+     ?COLUMN7(5, 10),
+     ?COLUMN7(6, 10),
+     ?COLUMN7(7, 10),
+     ?COLUMN7(8, 10),
+     ?COLUMN7(9, 10),
+     ?COLUMN7(10, 10),
+     ?COLUMN7(11, 10),
+     ?COLUMN10(12, 10),
+     ?COLUMN10(13, 10),
+     ?COLUMN10(14, 10),
+     ?COLUMN10(15, 10),
+     ?COLUMN10(16, 10)
+    ];
+labs(epm2210) ->
+    [?COLUMN10(1, 13),
+     ?COLUMN10(2, 13),
+     ?COLUMN10(3, 13),
+     ?COLUMN10(4, 13),
+     ?COLUMN10(5, 13),
+     ?COLUMN10(6, 13),
+     ?COLUMN10(7, 13),
+     ?COLUMN10(8, 13),
+     ?COLUMN10(9, 13),
+     ?COLUMN10(10, 13),
+     ?COLUMN10(11, 13),
+     ?COLUMN10(12, 13),
+     ?COLUMN10(13, 13),
+     ?COLUMN13(14, 13),
+     ?COLUMN13(15, 13),
+     ?COLUMN13(16, 13),
+     ?COLUMN13(17, 13),
+     ?COLUMN13(18, 13),
+     ?COLUMN13(19, 13),
+     ?COLUMN13(20, 13)
+    ].
 
