e6f13875bb19d6d336669c0fff431a6b446722cc
Michael Taylor <michael@emte.net.au>
2023-02-06 10:34:45 +1100

    Add fuses:name() type

--- experiments/src/fuses.erl
+++ experiments/src/fuses.erl
@@ -6,8 +6,10 @@
 -export([subtract/2]).
 
 -export_type([fuse/0]).
+-export_type([name/0]).
 
 -type fuse() :: non_neg_integer().
+-type name() :: term().
 
 %%====================================================================
 %% diff
