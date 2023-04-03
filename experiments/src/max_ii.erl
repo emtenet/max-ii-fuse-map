2fdfea349893d44e6dc784f18b79e7a3d82d3960
Michael Taylor <michael@emte.net.au>
2023-04-03 19:00:26 +1000

    Add more common types

--- experiments/src/max_ii.erl
+++ experiments/src/max_ii.erl
@@ -7,28 +7,40 @@
 -export_type([n/0]).
 -export_type([i/0]).
 
+-export_type([mux3/0]).
+-export_type([mux4/0]).
+
 -export_type([le_buffer/0]).
--export_type([le_buffer_index/0]).
 
 -export_type([c4/0]).
+-export_type([c4_block/0]).
 -export_type([c4_index/0]).
+-export_type([c4_select/0]).
 
 -export_type([r4/0]).
+-export_type([r4_block/0]).
 -export_type([r4_index/0]).
+-export_type([r4_select/0]).
 
 -type x() :: 0..21.
 -type y() :: 0..13.
 -type n() :: 0..9.
 -type i() :: 0..3.
 
--type le_buffer() :: {le_buffer, x(), y(), 0, le_buffer_index()}.
--type le_buffer_index() :: 0..19.
+-type mux3() :: mux0 | mux1 | mux2.
+-type mux4() :: mux0 | mux1 | mux2 | mux3.
 
--type c4() :: {c4, x(), y(), 0, c4_index()}.
--type c4_index() :: 0..63.
+-type le_buffer() :: {le_buffer, x(), y(), 0, 0..19}.
 
--type r4() :: {r4, x(), y(), 0, r4_index()}.
--type r4_index() :: 0..63.
+-type c4() :: {c4, x(), y(), 0, 0..63}.
+-type c4_block() :: {c4, max_ii:x(), max_ii:y()}.
+-type c4_index() :: {mux, 0..13}.
+-type c4_select() :: direct_link | {mux4(), mux3()}.
+
+-type r4() :: {r4, x(), y(), 0, 0..63}.
+-type r4_block() :: {r4, max_ii:x(), max_ii:y()}.
+-type r4_index() :: {mux, 0..15}.
+-type r4_select() :: direct_link | {mux4(), mux3()}.
 
 %%====================================================================
 %% n_list
