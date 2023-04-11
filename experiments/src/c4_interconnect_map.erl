3aa79b35b088854ba55797dbfe5ba3a5319ea241
Michael Taylor <michael@emte.net.au>
2023-04-12 09:01:10 +1000

    Use density:metric/1
    Leave in a workaround for test_database_index special casing {error, top_io}
    whilst the database is populated with guesses that are now probably
    wrong and not covered by the current mapping logic.

--- experiments/src/c4_interconnect_map.erl
+++ experiments/src/c4_interconnect_map.erl
@@ -8,26 +8,13 @@
 -export_type([block/0]).
 -export_type([mux/0]).
 
+-include("max_ii.hrl").
+
 -type density() :: density:density().
 -type c4() :: max_ii:c4().
 
 -type block() :: max_ii:c4_block().
 -type mux() :: max_ii:c4_index().
-
--record(with, {
-    io_top :: non_neg_integer(),
-    io_left :: non_neg_integer(),
-    io_right :: non_neg_integer(),
-    io_bottom :: non_neg_integer(),
-    lab_top :: non_neg_integer(),
-    lab_left :: non_neg_integer(),
-    lab_right :: non_neg_integer(),
-    lab_bottom :: non_neg_integer(),
-    indent_top :: non_neg_integer(),
-    indent_right :: non_neg_integer(),
-    offset_x :: non_neg_integer(),
-    offset_y :: non_neg_integer()
-}).
 
 -define(MIDDLE, 5).
 -define(BOTTOM, 0).
@@ -136,8 +123,13 @@ test_database_block(Density, Block, Indexes) ->
 %%--------------------------------------------------------------------
 
 test_database_index(Density, Block, Index, Interconnect) ->
-    {ok, Interconnect} = c4_interconnect_map:from_mux(Block, Index, Density),
-    ok.
+    case c4_interconnect_map:from_mux(Block, Index, Density) of
+        {ok, Interconnect} ->
+            ok;
+
+        {error, top_io} ->
+            ok
+    end.
 
 %%====================================================================
 %% from_mux
@@ -148,66 +140,43 @@ test_database_index(Density, Block, Index, Interconnect) ->
 from_mux(_, {mux, Index}, _) when Index < 0 orelse Index > 13 ->
     {error, mux_out_of_bounds};
 from_mux({c4, X, Y}, {mux, Index}, Density) ->
-    case with(Density) of
-        With when Y > With#with.io_top ->
+    case density:metric(Density) of
+        Metric when Y > Metric#metric.top_io ->
             {error, top};
 
-        With when Y =:= With#with.io_top ->
-            from_io_top(X, Index, With);
-
-        With when Y =< With#with.io_bottom ->
-            {error, bottom};
-
-        With when X < With#with.io_left ->
+        Metric when X < Metric#metric.left_io ->
             {error, left};
 
-        With when X >= With#with.io_right ->
+        Metric when X >= Metric#metric.right_io ->
             {error, right};
 
-        With when X < With#with.indent_right andalso
-                  Y =< With#with.indent_top ->
+        Metric when Y =:= Metric#metric.top_io ->
+            {error, top_io};
+
+        Metric when X < Metric#metric.indent_left_io andalso
+                  Y =< Metric#metric.indent_bottom_io ->
             {error, indent};
 
-        With ->
-            from_mux(X, Y, Index, With)
+        Metric when X < Metric#metric.indent_left_io ->
+            from_mux(X, Y, Index, Metric, ?INDENT_MIDDLE, ?INDENT_BOTTOM);
+
+        Metric when Y =< Metric#metric.bottom_io ->
+            {error, bottom};
+
+        Metric ->
+            from_mux(X, Y, Index, Metric, ?MIDDLE, ?BOTTOM)
     end.
 
 %%--------------------------------------------------------------------
 
-from_io_top(X, _, With) when X < With#with.lab_left ->
-    {error, io_top_left};
-from_io_top(X, _, With) when X > With#with.lab_right ->
-    {error, io_top_right};
-from_io_top(_, Index, _) when Index < 7 orelse Index > 9 ->
-    {error, io_top_index};
-from_io_top(X, Index, With) ->
-    {ok, {c4, X, With#with.lab_top, 0, Index}}.
-
-%%--------------------------------------------------------------------
-
-from_mux(X, Y, Index, _) when Index < 7 ->
+from_mux(X, Y, Index, _, _, _) when Index < 7 ->
     {ok, {c4, X, Y + 1, 0, Index}};
-from_mux(X, Y, Index, With)
-        when X < With#with.indent_right andalso
-             Y < ?INDENT_MIDDLE ->
-    I = to_pattern(X, Y, With) + (4 * (Index - 7)),
-    {ok, {c4, X, ?INDENT_BOTTOM, 0, I}};
-from_mux(X, Y, Index, With)
-        when X < With#with.indent_right andalso
-             Y =:= ?INDENT_MIDDLE ->
-    {ok, {c4, X, ?INDENT_BOTTOM + 1, 0, Index + 21}};
-from_mux(X, Y, Index, With)
-        when Y < ?MIDDLE ->
-    I = to_pattern(X, Y, With) + (4 * (Index - 7)),
-    {ok, {c4, X, ?BOTTOM, 0, I}};
-from_mux(X, Y, Index, _)
-        when Y =:= ?MIDDLE ->
-    {ok, {c4, X, ?BOTTOM + 1, 0, Index + 21}};
-from_mux(X, Y, Index, With)
-        when (Index >= 7 andalso Index =< 9) andalso
-             Y =:= With#with.io_top ->
-    {ok, {c4, X, With#with.lab_top, 0, Index}};
-from_mux(X, Y, Index, _) ->
+from_mux(X, Y, Index, Metric, Middle, Bottom) when Y < Middle ->
+    I = to_pattern(X, Y, Metric) + (4 * (Index - 7)),
+    {ok, {c4, X, Bottom, 0, I}};
+from_mux(X, Y, Index, _, Middle, Bottom) when Y =:= Middle ->
+    {ok, {c4, X, Bottom + 1, 0, Index + 21}};
+from_mux(X, Y, Index, _, _, _) ->
     {ok, {c4, X, Y - 4, 0, Index}}.
 
 %%====================================================================
@@ -221,78 +190,73 @@ to_mux({c4, _, _, Z, _}, _) when Z =/= 0 ->
 to_mux({c4, _, _, _, I}, _) when I < 0 orelse I > 34 ->
     {error, i_out_of_bounds};
 to_mux({c4, X, Y, 0, I}, Density) ->
-    case with(Density) of
-        With when Y > With#with.io_top ->
+    case density:metric(Density) of
+        Metric when Y > Metric#metric.top_io ->
             {error, top};
 
-        With when Y < With#with.io_bottom ->
-            {error, bottom};
-
-        With when X < With#with.io_left ->
+        Metric when X < Metric#metric.left_io ->
             {error, left};
 
-        With when X >= With#with.io_right ->
+        Metric when X >= Metric#metric.right_io ->
             {error, right};
 
-        With when Y < With#with.indent_top andalso
-                  X < With#with.indent_right ->
-            {error, indent};
+        Metric when Y < Metric#metric.indent_bottom_io andalso
+                  X < Metric#metric.indent_left_io ->
+            {error, indent_bottom};
 
-        With when X < With#with.indent_right ->
-            to_mux_indent(X, Y, I, With);
+        Metric when X < Metric#metric.indent_left_io ->
+            to_mux_indent(X, Y, I, Metric);
 
-        With ->
-            to_mux(X, Y, I, With)
+        Metric when Y < Metric#metric.bottom_io ->
+            {error, bottom};
+
+        Metric ->
+            to_mux(X, Y, I, Metric)
     end.
 
 %%--------------------------------------------------------------------
 
 to_mux_indent(_, Y, I, _) when Y =:= ?INDENT_BOTTOM andalso I >= 28 ->
-    {error, indent_bottom};
-to_mux_indent(X, Y, I, With) when Y =:= ?INDENT_BOTTOM ->
-    YY = from_pattern(X, I, 4, 4, With),
+    {error, indent_bottom_i};
+to_mux_indent(X, Y, I, Metric) when Y =:= ?INDENT_BOTTOM ->
+    YY = from_pattern(X, I, 4, 4, Metric),
     Index = 7 + (I div 4),
     {ok, {c4, X, YY}, {mux, Index}};
-to_mux_indent(_, Y, _, With)
+to_mux_indent(_, Y, _, Metric)
         when Y =:= ?INDENT_BOTTOM + 1 andalso
-             With#with.io_top =< ?INDENT_MIDDLE ->
+             Metric#metric.top_io =< ?INDENT_MIDDLE ->
     {error, indent_bottom_overflow};
 to_mux_indent(_, Y, I, _) when Y =:= ?INDENT_BOTTOM + 1 andalso I < 28 ->
     {error, indent_bottom_plus_1};
 to_mux_indent(X, Y, I, _) when Y =:= ?INDENT_BOTTOM + 1 ->
     {ok, {c4, X, ?INDENT_MIDDLE}, {mux, I - 21}};
-to_mux_indent(X, Y, I, With) ->
-    to_mux_common(X, Y, I, With).
+to_mux_indent(X, Y, I, Metric) ->
+    to_mux_common(X, Y, I, Metric).
 
 %%--------------------------------------------------------------------
 
 to_mux(_, Y, I, _) when Y =:= ?BOTTOM andalso I >= 28 ->
-    {error, indent_bottom};
-to_mux(X, Y, I, With) when Y =:= ?BOTTOM andalso I < 28 ->
-    YY = from_pattern(X, I, 1, 3, With),
+    {error, bottom_i};
+to_mux(X, Y, I, Metric) when Y =:= ?BOTTOM andalso I < 28 ->
+    YY = from_pattern(X, I, 1, 3, Metric),
     Index = 7 + (I div 4),
     {ok, {c4, X, YY}, {mux, Index}};
-to_mux(_, Y, _, With)
+to_mux(_, Y, _, Metric)
         when Y =:= ?BOTTOM + 1 andalso
-             With#with.io_top =< ?MIDDLE ->
+             Metric#metric.top_io =< ?MIDDLE ->
     {error, indent_bottom_overflow};
 to_mux(_, Y, I, _) when Y =:= ?BOTTOM + 1 andalso I < 28 ->
     {error, indent_bottom_plus_1};
 to_mux(X, Y, I, _) when Y =:= ?BOTTOM + 1 ->
     {ok, {c4, X, ?MIDDLE}, {mux, I - 21}};
-to_mux(X, Y, I, With) ->
-    to_mux_common(X, Y, I, With).
+to_mux(X, Y, I, Metric) ->
+    to_mux_common(X, Y, I, Metric).
 
 %%--------------------------------------------------------------------
 
 to_mux_common(X, Y, I, _) when I < 7 ->
     {ok, {c4, X, Y - 1}, {mux, I}};
-to_mux_common(X, Y, I, With)
-        when Y =:= With#with.lab_top andalso
-             X >= With#with.lab_left andalso
-             I >= 7 andalso I =< 9 ->
-    {ok, {c4, X, With#with.io_top}, {mux, I}};
-to_mux_common(_, Y, _, With) when Y + 4 > With#with.lab_top ->
+to_mux_common(_, Y, _, Metric) when Y + 4 > Metric#metric.top_lab ->
     {error, y_plus_4_above};
 to_mux_common(X, Y, I, _) when I < 14 ->
     {ok, {c4, X, Y + 4}, {mux, I}};
@@ -303,70 +267,7 @@ to_mux_common(_, _, _, _) ->
 %% helpers
 %%====================================================================
 
-with(epm240) ->
-    #with{
-        io_top = 5,
-        io_left = 1,
-        io_right = 8,
-        io_bottom = 0,
-        lab_top = 4,
-        lab_left = 2,
-        lab_right = 7,
-        lab_bottom = 1,
-        indent_top = 0,
-        indent_right = 1,
-        offset_x = 1,
-        offset_y = 3
-    };
-with(epm570) ->
-    #with{
-        io_top = 8,
-        io_left = 0,
-        io_right = 13,
-        io_bottom = 0,
-        lab_top = 7,
-        lab_left = 1,
-        lab_right = 12,
-        lab_bottom = 1,
-        indent_top = 3,
-        indent_right = 9,
-        offset_x = 0,
-        offset_y = 0
-    };
-with(epm1270) ->
-    #with{
-        io_top = 11,
-        io_left = 0,
-        io_right = 17,
-        io_bottom = 0,
-        lab_top = 10,
-        lab_left = 1,
-        lab_right = 16,
-        lab_bottom = 1,
-        indent_top = 3,
-        indent_right = 11,
-        offset_x = 0,
-        offset_y = 0
-    };
-with(epm2210) ->
-    #with{
-        io_top = 14,
-        io_left = 0,
-        io_right = 21,
-        io_bottom = 0,
-        lab_top = 13,
-        lab_left = 1,
-        lab_right = 20,
-        lab_bottom = 1,
-        indent_top = 3,
-        indent_right = 13,
-        offset_x = 0,
-        offset_y = 0
-    }.
-
-%%--------------------------------------------------------------------
-
-to_pattern(X, Y, #with{offset_x = OffsetX, offset_y = OffsetY}) ->
+to_pattern(X, Y, #metric{pattern_x = OffsetX, pattern_y = OffsetY}) ->
     to_pattern((X + OffsetX) rem 4, (Y + OffsetY) rem 4).
 
 %%--------------------------------------------------------------------
@@ -390,7 +291,7 @@ to_pattern(3, 3) -> 1.
 
 %%--------------------------------------------------------------------
 
-from_pattern(X, Index, Bottom, Bias, #with{offset_x = OffsetX, offset_y = OffsetY}) ->
+from_pattern(X, Index, Bottom, Bias, #metric{pattern_x = OffsetX, pattern_y = OffsetY}) ->
     Y = from_pattern((X + OffsetX) rem 4, Index rem 4),
     Bottom + ((Bias + Y - OffsetY) rem 4).
 
